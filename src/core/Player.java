12d6564093326c46b639c76d3e88c1d580fc3104
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-17 23:51:09 +0300

    feat: added Player

---
+++ src/core/Player.java
@@ -0,0 +1,53 @@
+package core;
+
+import economic.BankAccount;
+import tower.Tower;
+import tower.TowerUpgradableCharacteristic;
+import utils.BuildingResponse;
+
+public class Player {
+    private final Builder builder;
+    private final BankAccount bankAccount;
+    private boolean frozen;
+    private int lives;
+
+    public Player(Builder builder) {
+        this.builder = builder;
+        this.bankAccount = new BankAccount();
+        frozen = false;
+        lives = 10;
+    }
+
+    public void reduceLives(int lives) {
+        this.lives -= lives;
+    }
+
+    public boolean placeTower(Class<? extends Tower> tower, Cell cell) {
+        BuildingResponse response = builder.buildTower(tower, cell, bankAccount.gold);
+        bankAccount.setGold(response.change());
+        return response.built();
+    }
+
+    public boolean upgradeTower(Tower tower, TowerUpgradableCharacteristic towerUpgrade) {
+        BuildingResponse response = builder.upgradeTower(tower,towerUpgrade, bankAccount.gold);
+        bankAccount.setGold(response.change());
+        return response.built();
+    }
+
+    // - /////////////////////////////////////////////////////////////
+    public BankAccount getBankAccount() {
+        return this.bankAccount;
+    }
+
+    public boolean frozen() {
+        return this.frozen;
+    }
+
+    public void freeze(boolean frozen) {
+        this.frozen = frozen;
+    }
+
+    public int getLives() {
+        return this.lives;
+    }
+}
