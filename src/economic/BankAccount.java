641ae82220f731527315b031150fe1820cfe506a
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-17 22:45:51 +0300

    feat: added BankAccount

---
+++ src/economic/BankAccount.java
@@ -0,0 +1,41 @@
+package economic;
+
+public class BankAccount {
+    public int gold;
+
+    public BankAccount() {
+        this.gold = 0;
+    }
+
+    public BankAccount(int gold) {
+        if (gold < 0) {
+            throw new IllegalArgumentException("Gold count cannot be negative");
+        }
+
+        this.gold = gold;
+    }
+
+    public void creditGold(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("Credit count cannot be negative");
+        }
+
+        this.gold += count;
+    }
+
+    public void writeOffGold(int count) {
+        if (gold < count) {
+            throw new IllegalArgumentException("Not enough gold");
+        }
+
+        if (count < 0) {
+            throw new IllegalArgumentException("Credit count cannot be negative");
+        }
+
+        this.gold -= count;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+}
---
+++ test/economic/BankAccountTest.java
@@ -0,0 +1,53 @@
+package economic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+    @Test
+    void writeOffGold() {
+        BankAccount bankAccount = new BankAccount(100);
+
+        bankAccount.writeOffGold(50);
+
+        int expectedGold = 50;
+        assertEquals(expectedGold, bankAccount.gold);
+    }
+
+    @Test
+    void writeOffGoldMoreThanHave() {
+        BankAccount bankAccount = new BankAccount(0);
+
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.writeOffGold(50));
+    }
+
+    @Test
+    void writeOffNegativeGold() {
+        BankAccount bankAccount = new BankAccount(0);
+
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.writeOffGold(-50));
+    }
+
+    @Test
+    void creditGold() {
+        BankAccount bankAccount = new BankAccount();
+
+        bankAccount.creditGold(50);
+
+        int expectedGold = 50;
+        assertEquals(expectedGold, bankAccount.gold);
+    }
+
+    @Test
+    void creditNegativeGold() {
+        BankAccount bankAccount = new BankAccount(0);
+
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.writeOffGold(-50));
+    }
+
+    @Test
+    void creatingNegativeGoldCountBankAccount() {
+        assertThrows(IllegalArgumentException.class, () -> new BankAccount(-50));
+    }
+}
