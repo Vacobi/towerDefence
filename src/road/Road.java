3b8d4b0414e085c1fe12092465553bb479b33e56
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-22 00:29:50 +0300

    feat: added road cells as field in Road class

--- src/road/Road.java
+++ src/road/Road.java
@@ -1,18 +1,29 @@
 package road;
 
+import core.RoadCell;
+
 import java.util.List;
 import java.util.Optional;
 
 public class Road {
-
     private final List<RoadSegment> road;
+    private final List<RoadCell> roadCells;
 
-    public Road(List<RoadSegment> road) {
+    public Road(List<RoadSegment> road, List<RoadCell> roadCells) {
         this.road = road;
+        this.roadCells = roadCells;
+    }
+
+    public Road(String path) {
+        RoadParser roadParser = new RoadParser(path);
+        this.road = roadParser.getRoadSegments();
+        this.roadCells = roadParser.getRoadCells();
     }
 
     public Road() {
-        this(RoadParser.extractRoadSegments("../../roads.txt"));
+        RoadParser roadParser = new RoadParser("road.txt");
+        this.road = roadParser.getRoadSegments();
+        this.roadCells = roadParser.getRoadCells();
     }
 
     public List<RoadSegment> getRoad() {
@@ -28,4 +39,11 @@ public class Road {
         return Optional.of(road.get(indexOfCurrent + 1));
     }
 
+    public List<RoadSegment> getRoadSegments() {
+        return road;
+    }
+
+    public List<RoadCell> getRoadCells() {
+        return roadCells;
+    }
 }
