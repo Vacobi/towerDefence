1802f3ca0c133419c8923cb93cef7f9b14608080
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-22 14:23:05 +0300

    delete: removed dependency of ProjectileType in Tower

--- src/factory/TowerFactory.java
+++ src/factory/TowerFactory.java
@@ -2,8 +2,6 @@ package factory;
 
 import core.Cell;
 import core.Field;
-import projectile.LinearMovingProjectile;
-import projectile.Projectile;
 import tower.*;
 import tower.utils.TowerGeographicalCharacteristics;
 import tower.utils.TowerShootingCharacteristics;
@@ -16,14 +14,12 @@ import java.util.concurrent.TimeUnit;
 
 public class TowerFactory {
     private final List<Direction> shotDirections;
-    private final Class<? extends Projectile> projectileType;
     private final TowerCharacteristicsValues initialCharacteristicValues;
     private final int levelsCount;
     private final Class<? extends Tower> towerType = Tower.class;
 
     public TowerFactory() {
         shotDirections = Arrays.stream(Direction.values()).toList();
-        projectileType = LinearMovingProjectile.class;
         initialCharacteristicValues = new TowerCharacteristicsValues(
                 20.0,
                 100.0,
@@ -40,7 +36,7 @@ public class TowerFactory {
                 field
         );
         TowerGeographicalCharacteristics tgc = new TowerGeographicalCharacteristics(cell, field);
-        TowerShootingCharacteristics tsc = new TowerShootingCharacteristics(shotDirections, projectileType, strategy);
+        TowerShootingCharacteristics tsc = new TowerShootingCharacteristics(shotDirections, strategy);
         TowerUpgradablePart tup = new TowerUpgradablePart(initialCharacteristicValues, levelsCount);
 
         return new Tower(
--- src/tower/utils/TowerShootingCharacteristics.java
+++ src/tower/utils/TowerShootingCharacteristics.java
@@ -8,23 +8,16 @@ import java.util.List;
 
 public class TowerShootingCharacteristics {
     private final List<Direction> shotDirections;
-    private final Class<? extends Projectile> projectileType;
     private final ShootingStrategy shootingStrategy;
 
     public TowerShootingCharacteristics(List<Direction> shotDirections,
-                                        Class<? extends Projectile> projectileType,
                                         ShootingStrategy shootingStrategy) {
         this.shotDirections = shotDirections;
-        this.projectileType = projectileType;
         this.shootingStrategy = shootingStrategy;
     }
 
     public List<Direction> shotDirections() {
         return shotDirections;
-    }
-
-    public Class<? extends Projectile> projectileType() {
-        return projectileType;
     }
 
     public ShootingStrategy shootingStrategy() {
