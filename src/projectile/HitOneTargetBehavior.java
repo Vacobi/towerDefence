23055b2e3f52640cbe8e91314c9965760a53b276
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-22 14:06:17 +0300

    fix: added late init of projectile in Behavior to let possibility of creating Projectile

--- src/projectile/HitOneTargetBehavior.java
+++ src/projectile/HitOneTargetBehavior.java
@@ -4,14 +4,30 @@ import monster.Monster;
 
 public class HitOneTargetBehavior implements ProjectileBehavior {
 
-    private final Projectile projectile;
+    private Projectile projectile;
 
     public HitOneTargetBehavior(Projectile projectile) {
         this.projectile = projectile;
     }
 
+    public HitOneTargetBehavior() {
+        ;
+    }
+
+    public void setProjectile(Projectile projectile) {
+        if (this.projectile != null) {
+            throw new IllegalStateException("Projectile already set");
+        }
+
+        this.projectile = projectile;
+    }
+
     @Override
     public void applyEffect(long currentTick) {
+        if (projectile == null) {
+            throw new IllegalStateException("Projectile not set");
+        }
+
         for (Monster monster : projectile.getField().getWave().getAliveMonsters()) {
             if (hitMonster(monster)) {
                 return;
