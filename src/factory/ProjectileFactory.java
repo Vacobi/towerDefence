f71434ef87c2f20b85c49840affd9f5d67c2e186
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-22 14:20:55 +0300

    refactor: refactored ProjectileFactory to correspond to new MovingProjectile

--- src/factory/ProjectileFactory.java
+++ src/factory/ProjectileFactory.java
@@ -1,10 +1,42 @@
 package factory;
 
+import collision.Hitbox;
 import core.Field;
-import projectile.Projectile;
+import projectile.*;
 import utils.Direction;
 import utils.Position;
 
-public abstract class ProjectileFactory {
-    public abstract Projectile createProjectile(Position position, Direction direction, Field field);
+public class ProjectileFactory {
+    private final int defaultSpeed;
+    private final int defaultMaxDistance;
+    private final int defaultDamage;
+    private final int defaultWidth;
+    private final int defaultHeight;
+
+    public ProjectileFactory() {
+        defaultSpeed = 20;
+        defaultMaxDistance = 100;
+        defaultDamage = 20;
+        defaultWidth = 10;
+        defaultHeight = 10;
+    }
+
+    public MovingProjectile createMovingProjectile(Position position, Direction direction, Field field) {
+        LinearMovingProjectileStrategy movingStrategy = new LinearMovingProjectileStrategy(null, defaultSpeed, defaultMaxDistance, direction);
+        HitOneTargetBehavior behavior = new HitOneTargetBehavior();
+
+        Hitbox hitbox = new Hitbox(
+                position.getX() - defaultWidth / 2,
+                position.getY() + defaultHeight / 2,
+                defaultWidth,
+                defaultHeight,
+                0);
+        MovingProjectile projectile = new MovingProjectile(movingStrategy, hitbox, defaultDamage, position, behavior, field);
+
+        movingStrategy.setProjectile(projectile);
+        behavior.setProjectile(projectile);
+
+        return projectile;
+    }
+
 }
