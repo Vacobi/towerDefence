6fafb749c1a973984858a1a1683eb9cb851b82fc
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-01 00:32:28 +0300

    feat: added clone method in MovingProjectileStrategy interface

--- src/projectile/MovingProjectileStrategy.java
+++ src/projectile/MovingProjectileStrategy.java
@@ -1,5 +1,9 @@
 package projectile;
 
+import utils.Direction;
+
 public interface MovingProjectileStrategy {
     void move(long currentTick);
+
+    MovingProjectileStrategy clone(Direction direction);
 }
