ab64072be94f774132036889a6049a2f8189dc8c
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-11 22:10:34 +0300

    refactor: reworked ShootingStrategy interface

--- src/tower/ShootingStrategy.java
+++ src/tower/ShootingStrategy.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public interface ShootingStrategy<T extends Projectile> {
     List<T> shoot(long currentTick);
+
+    ShootingStrategy<T> clone();
+
+    void setTower(Tower<T> tower);
 }
