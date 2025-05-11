e2e6656b349a854dabbdae84cefb1d12438c5c72
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-11 21:34:53 +0300

    feat: added method of converting direction to radians

--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -4,5 +4,14 @@ public enum Direction {
     NORTH,
     WEST,
     SOUTH,
-    EAST
+    EAST;
+
+    public static double toRadians(Direction direction) {
+        return switch (direction) {
+            case NORTH -> -Math.PI / 2;
+            case WEST -> Math.PI;
+            case SOUTH -> Math.PI / 2;
+            case EAST -> 0;
+        };
+    }
 }
