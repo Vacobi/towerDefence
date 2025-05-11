30075c881d857caf2e67a84c1b288ec2bc8b66da
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-11 22:28:51 +0300

    refactor: reworked DirectionalShootingStrategy class

---
+++ src/tower/DirectionalShootingStrategy.java
@@ -0,0 +1,61 @@
+package tower;
+
+import projectile.DirectionalProjectile;
+import utils.Direction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DirectionalShootingStrategy implements ShootingStrategy<DirectionalProjectile>{
+    private Long lastShootTime;
+
+    private Tower<DirectionalProjectile> tower;
+
+    public DirectionalShootingStrategy(Tower<DirectionalProjectile> tower) {
+        this.tower = tower;
+        lastShootTime = System.currentTimeMillis();
+    }
+
+    public DirectionalShootingStrategy() {
+        this(null);
+    }
+
+    @Override
+    public void setTower(Tower<DirectionalProjectile> tower) {
+        if (this.tower != null) {
+            throw new IllegalArgumentException("Tower already assigned to this shooting strategy");
+        }
+
+        this.tower = tower;
+        lastShootTime -= tower.characteristicValues().shootingDelay();
+    }
+
+    @Override
+    public List<DirectionalProjectile> shoot(long currentTick) {
+        if (currentTick - lastShootTime < tower.characteristicValues().shootingDelay()) {
+            return Collections.emptyList();
+        }
+
+        if (tower == null) {
+            throw new IllegalStateException("Tower has not been set");
+        }
+
+        List<DirectionalProjectile> projectiles = new ArrayList<>();
+        for (Direction direction : tower.getShotDirections()) {
+            DirectionalProjectile projectile = tower.getTypicalProjectile().clone(
+                    tower.getCell().getGlobalPosition(),
+                    direction
+            );
+            projectiles.add(projectile);
+        }
+
+        lastShootTime = currentTick;
+        return projectiles;
+    }
+
+    @Override
+    public ShootingStrategy<DirectionalProjectile> clone() {
+        return new DirectionalShootingStrategy(null);
+    }
+}
