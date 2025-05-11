bf95d5b3f79f4c5c277e39b6d5e53f7d0091267c
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-11 22:32:10 +0300

    feat: added LaserBehavior

---
+++ src/projectile/behavior/LaserBehavior.java
@@ -0,0 +1,68 @@
+package projectile.behavior;
+
+import monster.Monster;
+import projectile.LaserProjectile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LaserBehavior implements ProjectileBehavior<LaserProjectile> {
+
+    private LaserProjectile projectile;
+
+    private long startTime;
+
+    private final Map<Monster, Long> lastDamageTimes;
+
+    public LaserBehavior(LaserProjectile projectile) {
+        this.projectile = projectile;
+
+        startTime = System.currentTimeMillis();
+
+        lastDamageTimes = new HashMap<>();
+    }
+
+    public LaserBehavior() {
+        this(null);
+    }
+
+    @Override
+    public void applyEffect(long currentTick) {
+        if (currentTick - startTime > projectile.getActiveTime()) {
+            projectile.deactivate();
+            return;
+        }
+
+        for (Monster monster : projectile.getField().getWave().getAliveMonsters()) {
+            if (projectile.collidesWith(monster) && canDamage(monster, currentTick)) {
+                monster.applyDamage(projectile.getDamage());
+
+                if (monster.alive()) {
+                    lastDamageTimes.remove(monster);
+                } else {
+                    lastDamageTimes.put(monster, currentTick);
+                }
+            }
+        }
+    }
+
+    private boolean canDamage(Monster monster, long currentTick) {
+        return !lastDamageTimes.containsKey(monster) ||
+                currentTick - lastDamageTimes.get(monster) > projectile.getDamageCooldown();
+    }
+
+    @Override
+    public LaserBehavior clone() {
+        return new LaserBehavior();
+    }
+
+    @Override
+    public void setProjectile(LaserProjectile projectile) {
+        this.projectile = projectile;
+    }
+
+    @Override
+    public LaserProjectile getProjectile() {
+        return projectile;
+    }
+}
