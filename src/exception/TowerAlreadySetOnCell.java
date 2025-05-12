4d2022421575fe454c3b73f6e09f4e7d81fda688
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-13 00:27:48 +0300

    delete: removed unused exception

--- src/exception/TowerAlreadySetOnCell.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-import tower.Tower;
-
-public class TowerAlreadySetOnCell extends RuntimeException {
-    public TowerAlreadySetOnCell(Tower tower) {
-        super("Tower " + tower + " already set on cell");
-    }
-}
