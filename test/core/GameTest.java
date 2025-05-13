99ab7ae9100e81a84bb75951b09c05f363789cb1
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-13 10:28:07 +0300

    test: added test for game initializing

---
+++ test/core/GameTest.java
@@ -0,0 +1,15 @@
+package core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void startWave() {
+        Game game = new Game();
+
+        game.startWave();
+    }
+}
