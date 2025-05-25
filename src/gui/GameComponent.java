b8260656a9e2d56c5908761d1d1aa6634f46f88a
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-26 00:35:03 +0300

    feat: added abstract GameComponent

---
+++ src/gui/GameComponent.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class GameComponent<T> extends JComponent {
+    protected final T model;
+
+    public GameComponent(T model, int width, int height) {
+        this.model = model;
+        setSize(width, height);
+    }
+
+    @Override
+    public abstract void paintComponent(Graphics g);
+
+    public T getModel() {
+        return model;
+    }
+}
