e74d28cb4bed8318cfe30acd93a041e8755852a2
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-26 00:07:28 +0300

    refactor: changed type of 'availableTowersWithPrices' from HashMap to LinkedHashMap to keep order

--- src/tower/TowersCatalogue.java
+++ src/tower/TowersCatalogue.java
@@ -43,7 +43,7 @@ public class TowersCatalogue {
         projectileBehaviors = new HashMap<>();
         initializeProjectileBehaviors();
 
-        availableTowersWithPrices = new HashMap<>();
+        availableTowersWithPrices = new LinkedHashMap<>();
         initializeTowersWithPrices(field);
 
         upgradePrices = new HashMap<>();
