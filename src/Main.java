fc05e22381c51f766a1d2e1d53d89468672b1c4c
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-26 00:39:59 +0300

    feat: added Ui initialization in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import core.Game;
+import gui.GameWindow;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SwingUtilities.invokeLater(() -> {
+            Game game = new Game();
+            new GameWindow(game);
+        });
     }
 }
