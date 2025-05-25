08496d61be67a684aa3df9ac65ac0782ca48800a
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-26 00:32:48 +0300

    feat: added GameListener

--- src/events/GameListener.java
+++ src/events/GameListener.java
@@ -1,9 +1,14 @@
 package events;
 
 import core.Player;
+import core.Wave;
 
 public interface GameListener {
     void onPlayerWin(Player player);
 
     void onPlayerLose(Player player);
+
+    void onWaveStart(Wave wave);
+
+    void onWaveEnd(Wave wave);
 }
