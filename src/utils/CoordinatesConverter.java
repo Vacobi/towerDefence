e4f9ed0431112a995fba02d042d2bd9633664709
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-25 23:24:47 +0300

    refactor: CoordinatesConverter using cell size from AbstractCell

--- src/utils/CoordinatesConverter.java
+++ src/utils/CoordinatesConverter.java
@@ -1,8 +1,8 @@
 package utils;
 
+import core.AbstractCell;
+
 public class CoordinatesConverter {
-    private static final int CellPositionToGlobalPositionCoeffX = 30;
-    private static final int CellPositionToGlobalPositionCoeffY = 30;
 
     public static Position centerToLeftTop (Position center, int width, int height) {
         return new Position(center.getX() - width / 2, center.getY() + height / 2);
@@ -10,37 +10,20 @@ public class CoordinatesConverter {
 
     public static Position toGlobalCoordinates(Position position) {
         return new Position(
-                xToGlobalCoordinate(position.getX()),
-                yToGlobalCoordinate(position.getY())
+                toGlobalCoordinate(position.getX()),
+                toGlobalCoordinate(position.getY())
         );
     }
 
-    public static int xToGlobalCoordinate(int x) {
-        return x * CellPositionToGlobalPositionCoeffX;
+    public static int toGlobalCoordinate(int coordinate) {
+        return coordinate * AbstractCell.getSize();
     }
 
-    public static int yToGlobalCoordinate(int y) {
-        return y * CellPositionToGlobalPositionCoeffY;
-    }
-
-    public static int coeffConvertToGlobalX() {
-        return CellPositionToGlobalPositionCoeffX;
-    }
-
-    public static int coeffConvertToGlobalY() {
-        return CellPositionToGlobalPositionCoeffY;
-    }
-
-    public static int lengthOfSegment(int coordinatesCount, Direction direction) {
+    public static int lengthOfSegment(int coordinatesCount) {
         if (coordinatesCount < 0) {
             throw new IllegalArgumentException("Coordinates count must be greater than zero");
         }
 
-        switch (direction) {
-            case NORTH, SOUTH -> { return coordinatesCount * CellPositionToGlobalPositionCoeffY; }
-            case WEST, EAST -> {return coordinatesCount * CellPositionToGlobalPositionCoeffX;}
-        }
-
-        return coordinatesCount;
+        return coordinatesCount * AbstractCell.getSize();
     }
 }
