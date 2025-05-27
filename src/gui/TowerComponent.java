fd6b9a30b695f8036945605504750831c265db25
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-27 21:55:56 +0300

    feat: added sprites to towers (differs from type of projectile)

--- src/gui/TowerComponent.java
+++ src/gui/TowerComponent.java
@@ -1,21 +1,56 @@
 package gui;
 
+import gui.sprites.SpriteLoader;
+import projectile.LaserProjectile;
+import projectile.PlainProjectile;
+import projectile.Projectile;
 import tower.Tower;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TowerComponent extends GameComponent<Tower<?>> {
+
+    protected static Map<Class<? extends Projectile>, BufferedImage> sprites;
+
+    static {
+        sprites = new HashMap<>();
+
+        String pathToLaserTowerSprite = "/gui/sprites/laser_tower.png";
+        sprites.put(LaserProjectile.class, SpriteLoader.getSprite(pathToLaserTowerSprite));
+
+        String pathToDefaultTowerSprite = "/gui/sprites/tower.png";
+        sprites.put(PlainProjectile.class, SpriteLoader.getSprite(pathToDefaultTowerSprite));
+    }
+
+    protected BufferedImage sprite;
     public TowerComponent(Tower<?> model, int width, int height) {
         super(model,
                 width,
                 height
         );
+
+        sprite = sprites.get(model.getTypicalProjectile().getClass());
     }
 
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.BLACK);
-        g2.fillRect(0, 0, getWidth(), getHeight());
+
+        if (sprite != null) {
+            g2.drawImage(sprite,
+                    0,
+                    0,
+                    getWidth(),
+                    getHeight(),
+                    null
+            );
+        } else {
+            g2.setColor(Color.BLACK);
+            g2.fillRect(0, 0, getWidth(), getHeight());
+            g2.drawRect(0, 0, getWidth(), getHeight());
+        }
     }
 }
