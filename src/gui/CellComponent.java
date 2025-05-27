7920e0a5f30b88e3cba6d756d67d756605c6aee3
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-27 21:52:41 +0300

    feat: added sprite to cell

--- src/gui/CellComponent.java
+++ src/gui/CellComponent.java
@@ -1,10 +1,20 @@
 package gui;
 
 import core.Cell;
+import gui.sprites.SpriteLoader;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class CellComponent extends GameComponent<Cell> {
+
+    protected static BufferedImage sprite;
+
+    static {
+        String pathToGrassSprite = "/gui/sprites/grass_sprite.png";
+        sprite = SpriteLoader.getSprite(pathToGrassSprite);
+    }
+
     public CellComponent(Cell model, int width, int height) {
         super(model, width, height);
     }
@@ -12,9 +22,22 @@ public class CellComponent extends GameComponent<Cell> {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(new Color(40, 180, 40));
-        g2.fillRect(0, 0, getWidth(), getHeight());
+        int borderSize = 1;
+
+        if (sprite != null) {
+            g2.drawImage(
+                    sprite,
+                    borderSize, borderSize,
+                    getWidth() - 2 * borderSize, getHeight() - 2 * borderSize,
+                    null
+            );
+        } else {
+            g2.setColor(new Color(40, 180, 40));
+            g2.fillRect(0, 0, getWidth(), getHeight());
+        }
+
         g2.setColor(Color.BLACK);
-        g2.drawRect(0, 0, getWidth(), getHeight());
+        g2.setStroke(new BasicStroke(0.5F));
+        g2.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
     }
 }
