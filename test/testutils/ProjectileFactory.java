57b56107a73183f7b8f2c5b246997c5c80607c5d
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-05-28 21:28:15 +0300

    test: convert class's fields to method's variables

--- test/testutils/ProjectileFactory.java
+++ test/testutils/ProjectileFactory.java
@@ -9,33 +9,26 @@ import utils.Direction;
 import utils.Position;
 
 public class ProjectileFactory {
-    private final int defaultSpeed;
-    private final int defaultMaxDistance;
-    private final int defaultDamage;
-    private final int defaultWidth;
-    private final int defaultHeight;
-
-    public ProjectileFactory() {
-        defaultSpeed = 20;
-        defaultMaxDistance = 100;
-        defaultDamage = 20;
-        defaultWidth = 10;
-        defaultHeight = 10;
-    }
 
     public MovingProjectile createLinearMovingHitOneTargetProjectile(Position position, Direction direction, Field field) {
-        LinearMovingProjectileStrategy movingStrategy = new LinearMovingProjectileStrategy(defaultSpeed);
+        int speed = 20;
+        int maxDistance = 100;
+        int damage = 20;
+        int width = 10;
+        int height = 10;
+
+        LinearMovingProjectileStrategy movingStrategy = new LinearMovingProjectileStrategy(speed);
         HitOneTargetBehavior behavior = new HitOneTargetBehavior();
 
         HitboxParameters hitboxParameters = new HitboxParameters(
-                defaultWidth,
-                defaultHeight,
+                width,
+                height,
                 0
         );
         MovingProjectile projectile = new PlainProjectile(
                 hitboxParameters,
-                defaultDamage,
-                defaultMaxDistance,
+                damage,
+                maxDistance,
                 position,
                 behavior,
                 field,
