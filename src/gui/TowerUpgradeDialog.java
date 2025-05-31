cf395cbde70dfb71c78468acbaa3c4b27ca9b30d
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-01 00:00:25 +0300

    refactor: refactored TowerUpgradeDialog

--- src/gui/TowerUpgradeDialog.java
+++ src/gui/TowerUpgradeDialog.java
@@ -16,49 +16,58 @@ public class TowerUpgradeDialog extends JDialog {
         setLayout(new BorderLayout());
         JPanel upgradesPanel = new JPanel(new GridLayout(0, 1, 5, 5));
 
-        TowersCatalogue catalogue = player.getBuilder().getTowersCatalogue();
         for (TowerUpgradableCharacteristic characteristic : TowerUpgradableCharacteristic.values()) {
-            Optional<Integer> optionalLevel = tower.getLevelOfCharacteristic(characteristic);
-
-            int currentLevel = optionalLevel.get();
-            int maxLevel = tower.getLevelsUpgradeCount();
-
-            Optional<Integer> optionalCost = catalogue.getUpgradePrice(characteristic, currentLevel + 1);
-            JButton upgradeBtn;
-            if (optionalCost.isPresent()) {
-                int cost = optionalCost.get();
-                upgradeBtn = new JButton(
-                        String.format("%s (уровень %d/%d) - $%d", characteristic, currentLevel, maxLevel, cost)
-                );
-            } else {
-                upgradeBtn = new JButton(
-                        String.format("%s (уровень %d/%d)", characteristic, currentLevel, maxLevel)
-                );
-            }
-
-            if (!player.enoughGoldTUpgrade(tower, characteristic)) {
-                upgradeBtn.setEnabled(false);
-            }
-
-            upgradeBtn.addActionListener(e -> {
-                boolean upgraded = player.upgradeTower(tower, characteristic);
-                if (!upgraded) {
-                    JOptionPane.showMessageDialog(this, "Невозможно улучшить: недостаточно золота или максимальный уровень");
-                } else {
-                    JOptionPane.showMessageDialog(this, "Башня успешно улучшена!");
-                    dispose();
-                }
-            });
-
-            upgradesPanel.add(upgradeBtn);
+            JButton upgradeButton = createUpgradeButton(tower, player, characteristic);
+            upgradesPanel.add(upgradeButton);
         }
 
-        JButton closeBtn = new JButton("Закрыть");
-        closeBtn.addActionListener(e -> dispose());
+        setCloseButton();
 
         add(upgradesPanel, BorderLayout.CENTER);
-        add(closeBtn, BorderLayout.SOUTH);
         pack();
         setLocationRelativeTo(parent);
+    }
+
+    private void setCloseButton() {
+        JButton closeBtn = new JButton("Закрыть");
+        closeBtn.addActionListener(e -> dispose());
+        add(closeBtn, BorderLayout.SOUTH);
+    }
+
+    private JButton createUpgradeButton(Tower<? extends Projectile> tower, Player player, TowerUpgradableCharacteristic characteristic) {
+        Optional<Integer> optionalLevel = tower.getLevelOfCharacteristic(characteristic);
+
+        int currentLevel = optionalLevel.get();
+        int maxLevel = tower.getLevelsUpgradeCount();
+
+        TowersCatalogue catalogue = player.getBuilder().getTowersCatalogue();
+        Optional<Integer> optionalCost = catalogue.getUpgradePrice(characteristic, currentLevel + 1);
+        JButton upgradeBtn;
+        if (optionalCost.isPresent()) {
+            int cost = optionalCost.get();
+            upgradeBtn = new JButton(
+                    String.format("%s (уровень %d/%d) - $%d", characteristic, currentLevel, maxLevel, cost)
+            );
+        } else {
+            upgradeBtn = new JButton(
+                    String.format("%s (уровень %d/%d)", characteristic, currentLevel, maxLevel)
+            );
+        }
+
+        if (!player.enoughGoldTUpgrade(tower, characteristic)) {
+            upgradeBtn.setEnabled(false);
+        }
+
+        upgradeBtn.addActionListener(e -> {
+            boolean upgraded = player.upgradeTower(tower, characteristic);
+            if (!upgraded) {
+                JOptionPane.showMessageDialog(this, "Невозможно улучшить: недостаточно золота или максимальный уровень");
+            } else {
+                JOptionPane.showMessageDialog(this, "Башня успешно улучшена!");
+                dispose();
+            }
+        });
+
+        return upgradeBtn;
     }
 }
