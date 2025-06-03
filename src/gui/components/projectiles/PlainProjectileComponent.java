e955d4ecfa6e80e4d79ca44a0032492754eea4fa
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-04 01:17:21 +0300

    refactor: reformatted 'PlainProjectileComponent' class

--- src/gui/components/projectiles/PlainProjectileComponent.java
+++ src/gui/components/projectiles/PlainProjectileComponent.java
@@ -32,6 +32,8 @@ public class PlainProjectileComponent extends GameComponent<PlainProjectile> {
         setOpaque(false);
     }
 
+    //------------------------------------------------------------------------------------------------------------------
+
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
