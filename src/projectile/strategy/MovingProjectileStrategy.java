c016202aaac5629f7ff158bd5c2e21b1328bcb1f
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-04 00:38:06 +0300

    refactor: reformatted 'MovingProjectileStrategy' class

--- src/projectile/strategy/MovingProjectileStrategy.java
+++ src/projectile/strategy/MovingProjectileStrategy.java
@@ -5,9 +5,9 @@ import projectile.MovingProjectile;
 public interface MovingProjectileStrategy {
     void move(long currentTick);
 
+    MovingProjectileStrategy clone();
+
     void setProjectile(MovingProjectile projectile);
 
     MovingProjectile getProjectile();
-
-    MovingProjectileStrategy clone();
 }
