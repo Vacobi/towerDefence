93a7038021bdaf3f1f3cac77aa3536cde0061489
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-04 00:33:40 +0300

    refactor: refactored structure of 'Cell' class

--- src/core/Cell.java
+++ src/core/Cell.java
@@ -21,6 +21,23 @@ public class Cell extends AbstractCell implements Cloneable {
         listeners = new LinkedList<>();
     }
 
+    //------------------------------------------------------------------------------------------------------------------
+
+    @Override
+    public Cell clone() {
+        return new Cell(position());
+    }
+
+    public boolean canPlaceTower() {
+        return tower == null;
+    }
+
+    public boolean hasTower() {
+        return tower != null;
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
     public Tower getTower() {
         return tower;
     }
@@ -35,31 +52,20 @@ public class Cell extends AbstractCell implements Cloneable {
         fireTowerBuilt(tower);
     }
 
-    private void fireTowerBuilt(Tower<? extends Projectile> tower) {
-        CellEvent event = new CellEvent(this);
-        event.setTower(tower);
-
-        listeners.forEach(l -> l.onTowerBuilt(event));
-    }
-
-    public boolean canPlaceTower() {
-        return tower == null;
-    }
-
-    @Override
-    public Cell clone() {
-        return new Cell(position());
-    }
-
-    public boolean hasTower() {
-        return tower != null;
-    }
-
     public void addListener(CellListener listener) {
         listeners.add(listener);
     }
 
     public void removeListener(CellListener listener) {
         listeners.remove(listener);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    private void fireTowerBuilt(Tower<? extends Projectile> tower) {
+        CellEvent event = new CellEvent(this);
+        event.setTower(tower);
+
+        listeners.forEach(l -> l.onTowerBuilt(event));
     }
 }
