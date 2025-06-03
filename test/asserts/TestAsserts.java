e6655ade4fc4385d28eaca596de2facce9d5deb1
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-04 00:52:02 +0300

    refactor: removed towers asserts from 'TestAsserts'

--- test/asserts/TestAsserts.java
+++ test/asserts/TestAsserts.java
@@ -7,13 +7,9 @@ import projectile.DirectionalProjectile;
 import projectile.MovingProjectile;
 import projectile.Projectile;
 import road.RoadSegment;
-import tower.Tower;
-import tower.TowerCharacteristicsValues;
-import tower.TowerUpgradableCharacteristic;
 import utils.BuildingResponse;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,42 +63,6 @@ public class TestAsserts {
         assertEquals(expected.getStart(), actual.getStart());
         assertEquals(expected.getDirection(), actual.getDirection());
         assertEquals(expected.getLength(), actual.getLength());
-    }
-
-    public static void assertTowersEquals(Tower expected, Tower actual) {
-        assertTowerCharacteristicsEquals(expected.getCharacteristicValues(), actual.getCharacteristicValues());
-        assertCellsEquals(expected.getCell(), actual.getCell());
-        assertEquals(expected.getField(), actual.getField());
-        assertEquals(expected.getStrategy().getClass(), actual.getStrategy().getClass());
-        assertEquals(expected.getShotDirections(), actual.getShotDirections());
-        assertEquals(expected.getLevelsUpgradeCount(), actual.getLevelsUpgradeCount());
-        assertProjectilesEquals(expected.getTypicalProjectile(), actual.getTypicalProjectile());
-        assertEquals(expected.getCharacteristicLevels(), actual.getCharacteristicLevels());
-    }
-
-    public static void assertTowerCharacteristicsEquals(TowerCharacteristicsValues expected, TowerCharacteristicsValues actual) {
-        assertEquals(expected.getDamage(), actual.getDamage());
-        assertEquals(expected.getRange(), actual.getRange());
-        assertEquals(expected.shootingDelay(), actual.shootingDelay());
-    }
-
-    public static void assertTowersAfterUpgradeEquals(
-            Tower expectedTower,
-            Tower actualTower,
-            TowerCharacteristicsValues expectedCharacteristics,
-            Map<TowerUpgradableCharacteristic, Integer> characteristicLevels
-    ) {
-        assertCellsEquals(expectedTower.getCell(), actualTower.getCell());
-        assertEquals(expectedTower.getField(), actualTower.getField());
-        assertEquals(expectedTower.getStrategy().getClass(), actualTower.getStrategy().getClass());
-        assertEquals(expectedTower.getShotDirections(), actualTower.getShotDirections());
-        assertEquals(expectedTower.getLevelsUpgradeCount(), actualTower.getLevelsUpgradeCount());
-        assertProjectilesEquals(
-                expectedTower.getTypicalProjectile().clone(expectedCharacteristics.getDamage(), expectedCharacteristics.getRange()),
-                actualTower.getTypicalProjectile()
-        );
-        assertEquals(characteristicLevels, actualTower.getCharacteristicLevels());
-        assertTowerCharacteristicsEquals(expectedCharacteristics, actualTower.getCharacteristicValues());
     }
 
     public static void assertCellsEquals(Cell expected, Cell actual) {
