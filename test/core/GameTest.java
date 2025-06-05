8052677b136890e60a3fa4538844f61cc966c4de
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-06-05 15:21:42 +0300

    test: added tests for Game

--- test/core/GameTest.java
+++ test/core/GameTest.java
@@ -2,74 +2,198 @@ package core;
 
 import events.GameEvent;
 import events.GameListener;
+import events.WaveEvent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class GameTest implements GameListener {
+class GameTest {
 
-    private static int win = 0;
-    private static int lose = 0;
+    @Test
+    void waveStarts() {
+        Game game = new Game();
+        game.addGameListener(new GameListenerImpl());
 
-//    @Test
-//    void waveEndsByPlayerLose() throws InterruptedException {
-//        Game game = new Game();
-//        game.addGameListener(this);
-//
-//        game.startWave();
-//
-//        Thread.sleep(TimeUnit.MINUTES.toMillis(1));
-//
-//        System.out.println(game.getField().getWave().getAliveMonsters());
-//        assertEquals(1, lose);
-//    }
+        game.startWave();
+        expectedEvents.add(Event.WAVE_START);
 
-//    @Test
-//    void waveEndsByPlayerWin() throws InterruptedException {
-//        Game game = new Game();
-//        game.addGameListener(this);
-//
-//        game.startWave();
-//
-//        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
-//
-//        game.getField().getWave().getAliveMonsters().forEach(monster -> {
-//            monster.applyDamage(100);
-//        });
-//
-//        game.startWave();
-//
-//        Thread.sleep(TimeUnit.MINUTES.toMillis(10));
-//
-//        System.out.println(game.getField().getWave().getAliveMonsters());
-//        assertEquals(1, lose);
-//    }
-
-
-    @Override
-    public void onPlayerWin(GameEvent event) {
-        win++;
+        assertEquals(expectedEvents, events);
+        assertEquals(0, playerWins);
+        assertEquals(0, playerLoses);
+        assertEquals(1, waveStarts);
+        assertEquals(0, waveEnds);
+        assertEquals(0, waveChanges);
     }
 
-    @Override
-    public void onPlayerLose(GameEvent event) {
-        lose++;
+    @Test
+    void waveEnds() {
+        Game game = new Game();
+        game.addGameListener(new GameListenerImpl());
+
+        WaveEvent waveEvent = new WaveEvent(game.getField().getWave());
+        waveEvent.setWave(game.getField().getWave());
+
+        game.onWaveEnd(waveEvent);
+        expectedEvents.add(Event.WAVE_END);
+        expectedEvents.add(Event.WAVE_CHANGE);
+
+        assertEquals(2, game.getField().getWave().getNumber());
+        assertEquals(expectedEvents, events);
+        assertEquals(0, playerWins);
+        assertEquals(0, playerLoses);
+        assertEquals(0, waveStarts);
+        assertEquals(1, waveEnds);
+        assertEquals(1, waveChanges);
     }
 
-    @Override
-    public void onWaveStart(GameEvent event) {
+    @Test
+    void monsterDeath() {
+        Game game = new Game();
+        game.addGameListener(new GameListenerImpl());
 
+        WaveEvent waveEvent = new WaveEvent(game.getField().getWave());
+
+        int goldBefore = game.getPlayer().getBankAccount().getGold();
+        for (int i = 0; i < 1000; i++) {
+            game.onMonsterDeath(waveEvent);
+        }
+        int goldAfter = game.getPlayer().getBankAccount().getGold();
+
+        assertTrue(goldAfter > goldBefore);
+        assertEquals(expectedEvents, events);
+        assertEquals(0, playerWins);
+        assertEquals(0, playerLoses);
+        assertEquals(0, waveStarts);
+        assertEquals(0, waveEnds);
+        assertEquals(0, waveChanges);
     }
 
-    @Override
-    public void onWaveEnd(GameEvent event) {
+    @Test
+    void monsterReachedEnd() {
+        Game game = new Game();
+        game.addGameListener(new GameListenerImpl());
 
+        WaveEvent waveEvent = new WaveEvent(game.getField().getWave());
+        waveEvent.setWave(game.getField().getWave());
+
+        int livesBefore = game.getPlayer().getLives();
+        game.onMonsterReachedEnd(waveEvent);
+        int livesAfter = game.getPlayer().getLives();
+
+        assertEquals(livesBefore - 1, livesAfter);
+        assertEquals(expectedEvents, events);
+        assertEquals(0, playerWins);
+        assertEquals(0, playerLoses);
+        assertEquals(0, waveStarts);
+        assertEquals(0, waveEnds);
+        assertEquals(0, waveChanges);
     }
 
-    @Override
-    public void onWaveChange(GameEvent event) {
+    @Test
+    void playerLoses() {
+        Game game = new Game();
+        game.addGameListener(new GameListenerImpl());
 
+        WaveEvent waveEvent = new WaveEvent(game.getField().getWave());
+        waveEvent.setWave(game.getField().getWave());
+
+        int totalLives = game.getPlayer().getLives();
+        for (int i = 0; i < totalLives; i++) {
+            game.onMonsterReachedEnd(waveEvent);
+        }
+        expectedEvents.add(Event.PLAYER_LOSE);
+
+        assertEquals(0, game.getPlayer().getLives());
+        assertEquals(expectedEvents, events);
+        assertEquals(0, playerWins);
+        assertEquals(1, playerLoses);
+        assertEquals(0, waveStarts);
+        assertEquals(0, waveEnds);
+        assertEquals(0, waveChanges);
+    }
+
+    @Test
+    void playerWins() {
+        Game game = new Game();
+        game.addGameListener(new GameListenerImpl());
+
+        WaveEvent waveEvent = new WaveEvent(game.getField().getWave());
+        waveEvent.setWave(game.getField().getWave());
+
+        int livesBefore = game.getPlayer().getLives();
+        int totalWaves = game.getTotalWavesCount();
+        for (int i = 0; i < totalWaves; i++) {
+            game.onWaveEnd(waveEvent);
+            expectedEvents.add(Event.WAVE_END);
+            if (i != totalWaves - 1) {
+                expectedEvents.add(Event.WAVE_CHANGE);
+            }
+        }
+        expectedEvents.add(Event.PLAYER_WIN);
+
+        assertEquals(livesBefore, game.getPlayer().getLives());
+        assertEquals(expectedEvents, events);
+        assertEquals(1, playerWins);
+        assertEquals(0, playerLoses);
+        assertEquals(0, waveStarts);
+        assertEquals(totalWaves, waveEnds);
+        assertEquals(totalWaves - 1, waveChanges);
+    }
+
+    private enum Event {PLAYER_WIN, PLAYER_LOSE, WAVE_START, WAVE_END, WAVE_CHANGE}
+    private List<Event> events;
+    private List<Event> expectedEvents;
+
+    private int playerWins;
+    private int playerLoses;
+    private int waveStarts;
+    private int waveEnds;
+    private int waveChanges;
+    @BeforeEach
+    void setUp() {
+        events = new LinkedList<>();
+        expectedEvents = new LinkedList<>();
+        playerWins = 0;
+        playerLoses = 0;
+        waveStarts = 0;
+        waveEnds = 0;
+        waveChanges = 0;
+    }
+
+    private class GameListenerImpl implements GameListener {
+
+        @Override
+        public void onPlayerWin(GameEvent event) {
+            events.add(Event.PLAYER_WIN);
+            playerWins++;
+        }
+
+        @Override
+        public void onPlayerLose(GameEvent event) {
+            events.add(Event.PLAYER_LOSE);
+            playerLoses++;
+        }
+
+        @Override
+        public void onWaveStart(GameEvent event) {
+            events.add(Event.WAVE_START);
+            waveStarts++;
+        }
+
+        @Override
+        public void onWaveEnd(GameEvent event) {
+            events.add(Event.WAVE_END);
+            waveEnds++;
+        }
+
+        @Override
+        public void onWaveChange(GameEvent event) {
+            events.add(Event.WAVE_CHANGE);
+            waveChanges++;
+        }
     }
 }
